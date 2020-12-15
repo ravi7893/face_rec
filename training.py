0bf785ad3eb5c07b880354729b3b531e2d79c977
Ravi Charan <30321048+ravi7893@users.noreply.github.com>
2020-12-15 19:45:23 +0530

    third

---
+++ training.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+from PIL import Image
+import os
+
+
+path = 'dataset'
+recognizer = cv2.face.LBPHFaceRecognizer_create() 
+
+detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+def getImagesAndLabels(path):
+
+    imagePaths = [os.path.join(path,f) for f in os.listdir(path)]
+    facesamples=[]
+    ids=[]
+
+    for imagePath in imagePaths:
+        PIL_img=Image.open(imagePath).convert('L')
+        img_numpy = np.array(PIL_img,'uint8')
+
+        id = int(os.path.split(imagePath)[-1].split(".")[1])
+        faces = detector.detectMultiScale(img_numpy)
+
+        for (x,y,w,h) in faces:
+            facesamples.append(img_numpy[y:y+h,x:x+w])
+            ids.append(id)
+
+    return facesamples,ids
+
+print("\n [INFO] Training faces ....")
+faces,ids=getImagesAndLabels(path)
+recognizer.train(faces,np.array(ids))
+
+recognizer.write('Trainer/trainer.yml')
+
+print("\n [INFO] {0} faces trained.".format(len(np.unique(ids))))
+
